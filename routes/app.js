8bcc023054d54d3221fd3508cbe1d93993636814
Nikolay Ignatyev <noilgt@gmail.com>
2016-10-04 23:49:02 +0300

    Check Mongo data saving

--- routes/app.js
+++ routes/app.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+var Department = require('../models/department');
+
 router.get('/', function(req, res, next) {
     res.render('index');
+});
+
+// Draft for check data savings in MongoDB
+router.get('/departments', function(req, res, next) {
+    res.render('departments');
+});
+
+router.post('/add/department', function(req, res, next) {
+    var department = req.body.department;
+    var dep = new Department({
+        title: department
+    }).save();
+
+    res.redirect('/departments');
 });
 
 module.exports = router;
